374d3b9587291a8f2c6451413d79f48bd061e7d0
Etnan Lopez <etnan.lopez@maxikash.com>
2025-01-04 16:19:26 -0600

    database update

--- src/main/resources/database/clubleonesdb.sql
+++ src/main/resources/database/clubleonesdb.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS `club2`.`instalacion` (
   `descripcion` VARCHAR(200) NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 4
+AUTO_INCREMENT = 6
 DEFAULT CHARACTER SET = utf8mb3;
 
 
@@ -37,6 +37,9 @@ CREATE TABLE IF NOT EXISTS `club2`.`actividad` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(45) NULL DEFAULT NULL,
   `id_instalacion` INT NULL DEFAULT NULL,
+  `dia` DATETIME(6) NULL DEFAULT NULL,
+  `hora_inicio` TIME NULL DEFAULT NULL,
+  `hora_fin` TIME NULL DEFAULT NULL,
   `duracion` INT NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   INDEX `instalacion_ref_idx` (`id_instalacion` ASC) VISIBLE,
@@ -44,7 +47,24 @@ CREATE TABLE IF NOT EXISTS `club2`.`actividad` (
     FOREIGN KEY (`id_instalacion`)
     REFERENCES `club2`.`instalacion` (`id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 5
+AUTO_INCREMENT = 7
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `club2`.`tipo_evento`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `club2`.`tipo_evento` (
+  `id_tipo_evento` INT NOT NULL AUTO_INCREMENT,
+  `categoria` ENUM('FEMENIL', 'VARONIL') NULL DEFAULT NULL,
+  `descripcion` VARCHAR(100) NOT NULL,
+  `modalidad` ENUM('EQUIPO', 'INDIVIDUAL', 'RELEVOS') NULL DEFAULT NULL,
+  `nombre` VARCHAR(20) NOT NULL,
+  `participantes` INT NULL DEFAULT NULL,
+  `unidades` JSON NULL DEFAULT NULL,
+  PRIMARY KEY (`id_tipo_evento`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
 DEFAULT CHARACTER SET = utf8mb3;
 
 
@@ -61,7 +81,7 @@ CREATE TABLE IF NOT EXISTS `club2`.`administracion` (
   PRIMARY KEY (`id_Administrador`),
   UNIQUE INDEX `correo_UNIQUE` (`correo` ASC) VISIBLE)
 ENGINE = InnoDB
-AUTO_INCREMENT = 7
+AUTO_INCREMENT = 9
 DEFAULT CHARACTER SET = utf8mb3;
 
 
@@ -76,11 +96,10 @@ CREATE TABLE IF NOT EXISTS `club2`.`evento` (
   `fecha_inicio_evento` DATETIME NOT NULL,
   `fecha_fin_evento` DATETIME NOT NULL,
   `horario` VARCHAR(255) NOT NULL,
-  `modalidades` VARCHAR(50) NOT NULL,
-  `Categoria` VARCHAR(50) NOT NULL,
+  `modalidades` TINYINT NOT NULL,
+  `categoria` TINYINT NOT NULL,
   `costo` DECIMAL(10,2) NOT NULL DEFAULT '0.00',
-  `requisitos` LONGTEXT NOT NULL,
-  `reglas` LONGTEXT NOT NULL,
+  `detalles` LONGTEXT NOT NULL,
   `tipoevento` INT NULL DEFAULT NULL,
   `id_Entrenador` INT NULL DEFAULT NULL,
   `id_Administrador` INT NULL DEFAULT NULL,
@@ -88,6 +107,10 @@ CREATE TABLE IF NOT EXISTS `club2`.`evento` (
   PRIMARY KEY (`id_evento`),
   INDEX `id_administrador_idx` (`id_Administrador` ASC) VISIBLE,
   INDEX `id_entrenador_idx` (`id_Entrenador` ASC) VISIBLE,
+  INDEX `FKkl0sh4v4m9qq2o1y7catsx1v8` (`tipoevento` ASC) VISIBLE,
+  CONSTRAINT `FKkl0sh4v4m9qq2o1y7catsx1v8`
+    FOREIGN KEY (`tipoevento`)
+    REFERENCES `club2`.`tipo_evento` (`id_tipo_evento`),
   CONSTRAINT `id_administrador`
     FOREIGN KEY (`id_Administrador`)
     REFERENCES `club2`.`administracion` (`id_Administrador`),
@@ -95,7 +118,7 @@ CREATE TABLE IF NOT EXISTS `club2`.`evento` (
     FOREIGN KEY (`id_Entrenador`)
     REFERENCES `club2`.`administracion` (`id_Administrador`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 13
+AUTO_INCREMENT = 18
 DEFAULT CHARACTER SET = utf8mb3;
 
 
@@ -106,6 +129,8 @@ CREATE TABLE IF NOT EXISTS `club2`.`actividadevento` (
   `id_actividad` INT NOT NULL,
   `id_evento` INT NOT NULL,
   `horario` TIMESTAMP NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
   INDEX `eventoref_idx` (`id_evento` ASC) VISIBLE,
   INDEX `actividadref_idx` (`id_actividad` ASC) VISIBLE,
   CONSTRAINT `actividadref`
@@ -115,6 +140,7 @@ CREATE TABLE IF NOT EXISTS `club2`.`actividadevento` (
     FOREIGN KEY (`id_evento`)
     REFERENCES `club2`.`evento` (`id_evento`))
 ENGINE = InnoDB
+AUTO_INCREMENT = 3
 DEFAULT CHARACTER SET = utf8mb3;
 
 
@@ -127,7 +153,7 @@ CREATE TABLE IF NOT EXISTS `club2`.`equipo` (
   PRIMARY KEY (`id_Equipo`),
   UNIQUE INDEX `nombre_UNIQUE` (`nombre_equipo` ASC) VISIBLE)
 ENGINE = InnoDB
-AUTO_INCREMENT = 4
+AUTO_INCREMENT = 6
 DEFAULT CHARACTER SET = utf8mb3;
 
 
@@ -154,7 +180,7 @@ CREATE TABLE IF NOT EXISTS `club2`.`atleta` (
     FOREIGN KEY (`id_equipo`)
     REFERENCES `club2`.`equipo` (`id_Equipo`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 3
+AUTO_INCREMENT = 5
 DEFAULT CHARACTER SET = utf8mb3;
 
 
@@ -164,7 +190,9 @@ DEFAULT CHARACTER SET = utf8mb3;
 CREATE TABLE IF NOT EXISTS `club2`.`atleta_evento` (
   `id_atleta` INT NULL DEFAULT NULL,
   `id_evento` INT NULL DEFAULT NULL,
-  `participacion` JSON NULL DEFAULT NULL,
+  `participacion` VARCHAR(255) NULL DEFAULT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
   INDEX `id_atleta_idx` (`id_atleta` ASC) VISIBLE,
   INDEX `id_evento_idx` (`id_evento` ASC) VISIBLE,
   CONSTRAINT `id_atleta_i`
@@ -174,6 +202,7 @@ CREATE TABLE IF NOT EXISTS `club2`.`atleta_evento` (
     FOREIGN KEY (`id_evento`)
     REFERENCES `club2`.`evento` (`id_evento`))
 ENGINE = InnoDB
+AUTO_INCREMENT = 3
 DEFAULT CHARACTER SET = utf8mb3;
 
 
@@ -184,7 +213,8 @@ CREATE TABLE IF NOT EXISTS `club2`.`equipo_evento` (
   `id_equipo` INT NOT NULL,
   `id_evento` INT NOT NULL,
   `participacion` JSON NULL DEFAULT NULL,
-  `num_integrantes_evento` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
   INDEX `id_Equipo_idx` (`id_equipo` ASC) VISIBLE,
   INDEX `id_evento_idx` (`id_evento` ASC) VISIBLE,
   CONSTRAINT `id_equipo_t`
@@ -193,22 +223,6 @@ CREATE TABLE IF NOT EXISTS `club2`.`equipo_evento` (
   CONSTRAINT `id_evento_t`
     FOREIGN KEY (`id_evento`)
     REFERENCES `club2`.`evento` (`id_evento`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `club2`.`tipo_evento`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `club2`.`tipo_evento` (
-  `id_tipo_evento` INT NOT NULL AUTO_INCREMENT,
-  `categoria` ENUM('FEMENIL', 'VARONIL') NULL DEFAULT NULL,
-  `descripcion` VARCHAR(100) NOT NULL,
-  `modalidad` ENUM('EQUIPO', 'INDIVIDUAL', 'RELEVOS') NULL DEFAULT NULL,
-  `nombre` VARCHAR(20) NOT NULL,
-  `participantes` INT NULL DEFAULT NULL,
-  `unidades` JSON NULL DEFAULT NULL,
-  PRIMARY KEY (`id_tipo_evento`))
 ENGINE = InnoDB
 AUTO_INCREMENT = 2
 DEFAULT CHARACTER SET = utf8mb3;
