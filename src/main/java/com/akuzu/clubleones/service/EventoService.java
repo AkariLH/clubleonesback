0c57eb8c68de6bc74be841f324eca891c4eda302
Akari LH <akarilh1@gmail.com>
2025-01-11 19:45:19 -0600

    no me acuerdo

--- src/main/java/com/akuzu/clubleones/service/EventoService.java
+++ src/main/java/com/akuzu/clubleones/service/EventoService.java
@@ -9,12 +9,7 @@ import com.akuzu.clubleones.repository.TipoEventoRepository;
 import com.akuzu.clubleones.util.EstadoEvento;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
